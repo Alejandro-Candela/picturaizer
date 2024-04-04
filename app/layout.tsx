f6c13f924f6d0f6b626fe4ad400dbc67c8268e20
alex-candela <alex.candela@outlook.com>
2024-04-04 13:58:44 +0200

    meta updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,10 +21,19 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <ClerkProvider appearance={{
-          variables: {colorPrimary: '#624cf5'}
-        }}>
+        <ClerkProvider
+            appearance={{
+                variables: { colorPrimary: "#624cf5" },
+            }}
+        >
             <html lang="en">
+                <head>
+                    <meta name="twitter:card" content="A platform to create and modify images with AI" />
+                    <meta property="og:title" content="Picturaizer" />
+                    <meta property="og:description" content="OA platform to create and modify images with AI" />
+                    <meta property="og:image" content="https://picturaizer.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fpicturaizer-logo.png&w=384&q=75" />
+                    <meta property="og:url" content="https://picturaizer.vercel.app/" />
+                </head>
                 <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>{children}</body>
             </html>
         </ClerkProvider>
