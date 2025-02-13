74e4b2d62abc185be5ef12c8100db49dc4d118a2
alex-candela <alex.candela@outlook.com>
2025-02-13 23:15:02 +0100

    Add proprietary LICENSE.md and update SpeedInsights import

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const IBMPlex = IBM_Plex_Sans({
     subsets: ["latin"],
